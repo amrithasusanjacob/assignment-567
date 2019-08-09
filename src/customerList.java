9d9df9c6ae3bf3bf15343b6bca25561e2a49f422
Amritha Susan Jacob <Amritha Susan Jacob@DESKTOP-TC8FUTD>
2019-08-09 12:58:41 -0400

    hydro class implemented

--- src/customerList.java
+++ src/customerList.java
@@ -1,4 +1,65 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class customerList {
+public class customerList implements iDisplay 
+{
+
+	private ArrayList<customer> customers = new ArrayList<>();
+	
+	public void addCustomer(customer customer) 
+	{
+		this.customers.add(customer);
+	}
+
+	public customer getCustomerById(int id) 
+	{
+		customer c = null;
+		for (customer customer : customers) 
+		{
+			if (customer.getCustomerId() == id) 
+				c = customer;
+			
+		}
+
+		if (c == null) 
+		{
+			System.out.println("No customer found with ID : " + id);
+
+		}
+		return c;
+	}
+
+	@Override
+	public void display() 
+	{
+		Collections.sort(customers, new CustomerComparator());
+		for (customer customer : customers) 
+		{
+			customer.display();
+		}
+	}
+
+}
+
+class CustomerComparator implements Comparator<customer> 
+{
+
+	@Override
+	public int compare(customer c1, customer c2) 
+	{
+		if (c1.getCustomerId() < c2.getCustomerId()) 
+		{
+			return -1;
+		} 
+		else if (c1.getCustomerId() > c2.getCustomerId()) 
+		{
+			return 1;
+		}
+		else 
+		{
+			return 0;
+		}
+	}
 
 }
--- src/hydro.java
+++ src/hydro.java
@@ -1,4 +1,44 @@
+import java.util.Date;
 
-public class hydro {
+public class hydro extends bill 
+{
+	private String agencyName;
+	private int unitsConsumed;
 
+	public hydro(int billId, Date billDate, String billType, double totalbillAmount, String agencyName,int unitsConsumed) 
+	{
+		super(billId, billDate, billType, totalbillAmount);
+		this.agencyName = agencyName;
+		this.unitsConsumed = unitsConsumed;
+	}
+
+	
+	public String getAgencyName() 
+	{
+		return agencyName;
+	}
+
+	public void setAgencyName(String agencyName) 
+	{
+		this.agencyName = agencyName;
+	}
+
+	public int getUnitsConsumed() 
+	{
+		return unitsConsumed;
+	}
+
+	public void setUnitsConsumed(int unitsConsumed) 
+	{
+		this.unitsConsumed = unitsConsumed;
+	}
+
+	@Override
+	public void display() 
+	{
+		super.display();
+		System.out.println("Agency Name : " + agencyName);
+		System.out.println("Unit Consumed :" + unitsConsumed + " Units");
+		
+	}
 }
