9ebe558f90cf1634a28e7ae3a164cbe003ff6ed5
Amritha Susan Jacob <Amritha Susan Jacob@DESKTOP-TC8FUTD>
2019-08-09 12:41:21 -0400

    set bill class

--- src/bill.java
+++ src/bill.java
@@ -1,4 +1,66 @@
+ import java.util.Date;
+public class bill implements iDisplay
+{
+		private int billId;
+		private Date billDate;
+		private String billType;
+		private double totbillAmount;
+		
+		public bill(int billId, Date billDate, String billType, double totbillAmount) 
+		{
+			this.billId = billId;
+			this.billDate = billDate;
+			this.billType = billType;
+			this.totbillAmount = totbillAmount;
+		}
+		
+		public int getBillId() 
+		{
+			return billId;
+		}
 
-public class bill {
+		public void setBillId(int billId) 
+		{
+			this.billId = billId;
+		}
+
+		public Date getBillDate() 
+		{
+			return billDate;
+		}
+
+		public void setBillDate(Date billDate) 
+		{
+			this.billDate = billDate;
+		}
+
+		public String getBillType() 
+		{
+			return billType;
+		}
+
+		public void setBillType(String billType) 
+		{
+			this.billType = billType;
+		}
+
+		public double getTotalbillAmount() 
+		{
+			return totbillAmount;
+		}
+
+		public void setTotalbillAmount(double totalbillAmount)
+		{
+			this.totbillAmount = totalbillAmount;
+		}
+
+		@Override
+		public void display() 
+		{
+			System.out.println("Bill Id :" + billId);
+			System.out.println("Bill Date :" + billDate);
+			System.out.println("Bill Type :" + billType);
+			System.out.println("Total Bill Amount : $" + totbillAmount);
+		}
 
 }
