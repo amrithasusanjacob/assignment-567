238a0213786c59a06caec71d538e580abf42653a
Amritha Susan Jacob <Amritha Susan Jacob@DESKTOP-TC8FUTD>
2019-08-09 13:01:59 -0400

    internet class got implemented to get bill of internet used

--- src/internet.java
+++ src/internet.java
@@ -1,4 +1,43 @@
+import java.util.Date;
 
-public class internet {
+public class internet extends bill 
+{
+	private String providerName;
+	private double internetgbUsed;
+	
+	public internet(int billId, Date billDate, String billType, double totalbillAmount, String providerName,double internetgbUsed) 
+	{
+		super(billId, billDate, billType, totalbillAmount);
+		this.internetgbUsed = internetgbUsed;
+		this.providerName = providerName;
+	}
+
+	public String getProviderName() 
+	{
+		return providerName;
+	}
+
+	public void setProviderName(String providerName) 
+	{
+		this.providerName = providerName;
+	}
+
+	public double getInternetgbUsed() {
+		return internetgbUsed;
+	}
+
+	public void setInternetgbUsed(double internetgbUsed) 
+	{
+		this.internetgbUsed = internetgbUsed;
+	}
+	
+	@Override
+	public void display() 
+	{
+		super.display();
+		System.out.println("Name of Provider : " + providerName);
+		System.out.println("Internet GB Used : " + internetgbUsed + " GB");
+		
+	}
 
 }
